6c2dd9996b52bbfde2a0f20cdbba0a33ccaefcf8
SHAN <1045117960@qq.com>
2021-07-08 22:11:45 +0800

    添加注释

--- SinGAN/imresize.py
+++ SinGAN/imresize.py
@@ -7,22 +7,26 @@ from math import pi
 #from SinGAN.functions import torch2uint8, np2torch
 import torch
 
-
+# y=(x+1)/2 ,0<=y<=1 把输出限制在[0,1]之间
 def denorm(x):
     out = (x + 1) / 2
     return out.clamp(0, 1)
 
+# y=(x-0.5)*2 ,-1<=y<=1 把输出限制在[-1,1]之间
 def norm(x):
     out = (x - 0.5) * 2
     return out.clamp(-1, 1)
 
+# 加载到设备，选择cpu/gpu
 def move_to_gpu(t):
     if (torch.cuda.is_available()):
         t = t.to(torch.device('cuda'))
     return t
 
+# 将数据类型从np变为torch类型
 def np2torch(x,opt):
-    if opt.nc_im == 3:
+    # 改变数组索引
+    if opt.nc_im == 3: # 图像通道数
         x = x[:,:,:,None]
         x = x.transpose((3, 2, 0, 1))/255
     else:
@@ -37,15 +41,17 @@ def np2torch(x,opt):
     x = norm(x)
     return x
 
+# 将数据类型从torch变为uint8类型
 def torch2uint8(x):
     x = x[0,:,:,:]
+    # 将tensor的维度换位（改变索引）
     x = x.permute((1,2,0))
     x = 255*denorm(x)
     x = x.cpu().numpy()
     x = x.astype(np.uint8)
     return x
 
-
+# 缩放图像尺寸
 def imresize(im,scale,opt):
     #s = im.shape
     im = torch2uint8(im)
@@ -54,6 +60,7 @@ def imresize(im,scale,opt):
     #im = im[:, :, 0:int(scale * s[2]), 0:int(scale * s[3])]
     return im
 
+# 调整图像大小
 def imresize_to_shape(im,output_shape,opt):
     #s = im.shape
     im = torch2uint8(im)
@@ -62,203 +69,132 @@ def imresize_to_shape(im,output_shape,opt):
     #im = im[:, :, 0:int(scale * s[2]), 0:int(scale * s[3])]
     return im
 
-
 def imresize_in(im, scale_factor=None, output_shape=None, kernel=None, antialiasing=True, kernel_shift_flag=False):
-    # First standardize values and fill missing arguments (if needed) by deriving scale from output shape or vice versa
+    # 首先通过从输出形状导出比例来标准化值并填充缺失的参数（如果需要），反之亦然
     scale_factor, output_shape = fix_scale_and_size(im.shape, output_shape, scale_factor)
-
-    # For a given numeric kernel case, just do convolution and sub-sampling (downscaling only)
+    # 对于给定的数字内核情况，只需进行卷积和子采样（仅缩小）
     if type(kernel) == np.ndarray and scale_factor[0] <= 1:
         return numeric_kernel(im, kernel, scale_factor, output_shape, kernel_shift_flag)
-
-    # Choose interpolation method, each method has the matching kernel size
+    # 选择插值方法
     method, kernel_width = {
         "cubic": (cubic, 4.0),
         "lanczos2": (lanczos2, 4.0),
         "lanczos3": (lanczos3, 6.0),
         "box": (box, 1.0),
         "linear": (linear, 2.0),
-        None: (cubic, 4.0)  # set default interpolation method as cubic
+        None: (cubic, 4.0)  # 默认插值方法
     }.get(kernel)
-
-    # Antialiasing is only used when downscaling
+    # 抗锯齿在缩小图像时才使用
     antialiasing *= (scale_factor[0] < 1)
-
-    # Sort indices of dimensions according to scale of each dimension. since we are going dim by dim this is efficient
+    # 根据每个维度的比例对维度索引进行排序
     sorted_dims = np.argsort(np.array(scale_factor)).tolist()
-
-    # Iterate over dimensions to calculate local weights for resizing and resize each time in one direction
+    # 迭代维度，计算每次在一个方向上调整大小的局部权重
     out_im = np.copy(im)
     for dim in sorted_dims:
-        # No point doing calculations for scale-factor 1. nothing will happen anyway
+        # 比例因子为1时无意义
         if scale_factor[dim] == 1.0:
             continue
-
-        # for each coordinate (along 1 dim), calculate which coordinates in the input image affect its result and the
-        # weights that multiply the values there to get its result.
+        # 对于每个坐标（一个维度），计算输入图像中的哪些坐标影响其结果并将其值相乘得到权重
         weights, field_of_view = contributions(im.shape[dim], output_shape[dim], scale_factor[dim],
                                                method, kernel_width, antialiasing)
-
-        # Use the affecting position values and the set of weights to calculate the result of resizing along this 1 dim
+        # 计算一个维度上调整大小的结果
         out_im = resize_along_dim(out_im, dim, weights, field_of_view)
-
     return out_im
 
-
 def fix_scale_and_size(input_shape, output_shape, scale_factor):
-    # First fixing the scale-factor (if given) to be standardized the function expects (a list of scale factors in the
-    # same size as the number of input dimensions)
+    # 首先将比例因子（如果给定）固定为函数期望的标准化（与输入维度数量相同大小的比例因子列表）
     if scale_factor is not None:
-        # By default, if scale-factor is a scalar we assume 2d resizing and duplicate it.
+        # 如果scale-factor 是标量，定义为 2d
         if np.isscalar(scale_factor):
             scale_factor = [scale_factor, scale_factor]
-
-        # We extend the size of scale-factor list to the size of the input by assigning 1 to all the unspecified scales
+        # 将比例因子扩展到输入的大小
         scale_factor = list(scale_factor)
         scale_factor.extend([1] * (len(input_shape) - len(scale_factor)))
 
-    # Fixing output-shape (if given): extending it to the size of the input-shape, by assigning the original input-size
-    # to all the unspecified dimensions
+    # 扩展原始输入图像到输入形状的大小
     if output_shape is not None:
         output_shape = list(np.uint(np.array(output_shape))) + list(input_shape[len(output_shape):])
-
-    # Dealing with the case of non-give scale-factor, calculating according to output-shape. note that this is
-    # sub-optimal, because there can be different scales to the same output-shape.
     if scale_factor is None:
         scale_factor = 1.0 * np.array(output_shape) / np.array(input_shape)
 
-    # Dealing with missing output-shape. calculating according to scale-factor
+    # 根据比例因子计算，处理丢失的输出形状
     if output_shape is None:
         output_shape = np.uint(np.ceil(np.array(input_shape) * np.array(scale_factor)))
-
     return scale_factor, output_shape
 
-
 def contributions(in_length, out_length, scale, kernel, kernel_width, antialiasing):
-    # This function calculates a set of 'filters' and a set of field_of_view that will later on be applied
-    # such that each position from the field_of_view will be multiplied with a matching filter from the
-    # 'weights' based on the interpolation method and the distance of the sub-pixel location from the pixel centers
-    # around it. This is only done for one dimension of the image.
+    # 此函数计算一组“过滤器”和一组稍后会使用到的 field_of_view
+    # field_of_view 中的每个位置都会与匹配的过滤器相乘
+    # 'weights'基于插值方法和子像素到像素中心的距离
 
-    # When anti-aliasing is activated (default and only for downscaling) the receptive field is stretched to size of
-    # 1/sf. this means filtering is more 'low-pass filter'.
     fixed_kernel = (lambda arg: scale * kernel(scale * arg)) if antialiasing else kernel
     kernel_width *= 1.0 / scale if antialiasing else 1.0
-
-    # These are the coordinates of the output image
+    # 输出图像的坐标
     out_coordinates = np.arange(1, out_length+1)
-
-    # These are the matching positions of the output-coordinates on the input image coordinates.
-    # Best explained by example: say we have 4 horizontal pixels for HR and we downscale by SF=2 and get 2 pixels:
-    # [1,2,3,4] -> [1,2]. Remember each pixel number is the middle of the pixel.
-    # The scaling is done between the distances and not pixel numbers (the right boundary of pixel 4 is transformed to
-    # the right boundary of pixel 2. pixel 1 in the small image matches the boundary between pixels 1 and 2 in the big
-    # one and not to pixel 2. This means the position is not just multiplication of the old pos by scale-factor).
-    # So if we measure distance from the left border, middle of pixel 1 is at distance d=0.5, border between 1 and 2 is
-    # at d=1, and so on (d = p - 0.5).  we calculate (d_new = d_old / sf) which means:
-    # (p_new-0.5 = (p_old-0.5) / sf)     ->          p_new = p_old/sf + 0.5 * (1-1/sf)
+    # 这些是输出坐标在输入图像坐标上的匹配位置
     match_coordinates = 1.0 * out_coordinates / scale + 0.5 * (1 - 1.0 / scale)
-
-    # This is the left boundary to start multiplying the filter from, it depends on the size of the filter
+    # 过滤器的左边界
     left_boundary = np.floor(match_coordinates - kernel_width / 2)
-
-    # Kernel width needs to be enlarged because when covering has sub-pixel borders, it must 'see' the pixel centers
-    # of the pixels it only covered a part from. So we add one pixel at each side to consider (weights can zeroize them)
+    # 在每一侧添加一个像素
     expanded_kernel_width = np.ceil(kernel_width) + 2
-
-    # Determine a set of field_of_view for each each output position, these are the pixels in the input image
-    # that the pixel in the output image 'sees'. We get a matrix whos horizontal dim is the output pixels (big) and the
-    # vertical dim is the pixels it 'sees' (kernel_size + 2)
+    # 为每个输出位置确定一组field_of_view
     field_of_view = np.squeeze(np.uint(np.expand_dims(left_boundary, axis=1) + np.arange(expanded_kernel_width) - 1))
-
-    # Assign weight to each pixel in the field of view. A matrix whos horizontal dim is the output pixels and the
-    # vertical dim is a list of weights matching to the pixel in the field of view (that are specified in
-    # 'field_of_view')
+    # 为视野中的每个像素分配权重
     weights = fixed_kernel(1.0 * np.expand_dims(match_coordinates, axis=1) - field_of_view - 1)
-
-    # Normalize weights to sum up to 1. be careful from dividing by 0
+    # 权重归一化
     sum_weights = np.sum(weights, axis=1)
     sum_weights[sum_weights == 0] = 1.0
     weights = 1.0 * weights / np.expand_dims(sum_weights, axis=1)
-
-    # We use this mirror structure as a trick for reflection padding at the boundaries
+    # 使用镜像结构进行边界填充
     mirror = np.uint(np.concatenate((np.arange(in_length), np.arange(in_length - 1, -1, step=-1))))
     field_of_view = mirror[np.mod(field_of_view, mirror.shape[0])]
-
-    # Get rid of  weights and pixel positions that are of zero weight
+    # 摆脱零权重的权重和像素位置
     non_zero_out_pixels = np.nonzero(np.any(weights, axis=0))
     weights = np.squeeze(weights[:, non_zero_out_pixels])
     field_of_view = np.squeeze(field_of_view[:, non_zero_out_pixels])
 
-    # Final products are the relative positions and the matching weights, both are output_size X fixed_kernel_size
     return weights, field_of_view
 
-
 def resize_along_dim(im, dim, weights, field_of_view):
-    # To be able to act on each dim, we swap so that dim 0 is the wanted dim to resize
+    # 每一维度分开插值，交换索引，对维度进行调换
     tmp_im = np.swapaxes(im, dim, 0)
-
-    # We add singleton dimensions to the weight matrix so we can multiply it with the big tensor we get for
-    # tmp_im[field_of_view.T], (bsxfun style)
+    # 将单一维度添加到权重矩阵中，就可以将它与张量相乘
     weights = np.reshape(weights.T, list(weights.T.shape) + (np.ndim(im) - 1) * [1])
-
-    # This is a bit of a complicated multiplication: tmp_im[field_of_view.T] is a tensor of order image_dims+1.
-    # for each pixel in the output-image it matches the positions the influence it from the input image (along 1 dim
-    # only, this is why it only adds 1 dim to the shape). We then multiply, for each pixel, its set of positions with
-    # the matching set of weights. we do this by this big tensor element-wise multiplication (MATLAB bsxfun style:
-    # matching dims are multiplied element-wise while singletons mean that the matching dim is all multiplied by the
-    # same number
+    # tmp_im[field_of_view.T] 是 image_dims+1 阶的张量
     tmp_out_im = np.sum(tmp_im[field_of_view.T] * weights, axis=0)
-
-    # Finally we swap back the axes to the original order
+    # 维度调换回来
     return np.swapaxes(tmp_out_im, dim, 0)
 
-
 def numeric_kernel(im, kernel, scale_factor, output_shape, kernel_shift_flag):
-    # See kernel_shift function to understand what this is
     if kernel_shift_flag:
         kernel = kernel_shift(kernel, scale_factor)
-
-    # First run a correlation (convolution with flipped kernel)
+    # 做相关运算
     out_im = np.zeros_like(im)
     for channel in range(np.ndim(im)):
         out_im[:, :, channel] = filters.correlate(im[:, :, channel], kernel)
-
-    # Then subsample and return
+    # 采样
     return out_im[np.round(np.linspace(0, im.shape[0] - 1 / scale_factor[0], output_shape[0])).astype(int)[:, None],
                   np.round(np.linspace(0, im.shape[1] - 1 / scale_factor[1], output_shape[1])).astype(int), :]
 
-
 def kernel_shift(kernel, sf):
-    # There are two reasons for shifting the kernel:
-    # 1. Center of mass is not in the center of the kernel which creates ambiguity. There is no possible way to know
-    #    the degradation process included shifting so we always assume center of mass is center of the kernel.
-    # 2. We further shift kernel center so that top left result pixel corresponds to the middle of the sfXsf first
-    #    pixels. Default is for odd size to be in the middle of the first pixel and for even sized kernel to be at the
-    #    top left corner of the first pixel. that is why different shift size needed between od and even size.
-    # Given that these two conditions are fulfilled, we are happy and aligned, the way to test it is as follows:
-    # The input image, when interpolated (regular bicubic) is exactly aligned with ground truth.
+    # 移动核的两个原因：
+    # 1. 质心不在核的中心。
+    # 2. 使左上角像素对应于sfXsf的中间像素。 默认情况下，奇数大小位于第一个像素的中间，而偶数大小的内核位于第一个第一个像素的左上角。
+    # 如果两个条件都满足，测试方法：输入图像在插值时（常规双三次）与地面实况完全对齐。
 
-    # First calculate the current center of mass for the kernel
+    # 计算质心
     current_center_of_mass = measurements.center_of_mass(kernel)
-
-    # The second ("+ 0.5 * ....") is for applying condition 2 from the comments above
+    # 原因2
     wanted_center_of_mass = np.array(kernel.shape) / 2 + 0.5 * (sf - (kernel.shape[0] % 2))
-
-    # Define the shift vector for the kernel shifting (x,y)
+    # 移位向量
     shift_vec = wanted_center_of_mass - current_center_of_mass
-
-    # Before applying the shift, we first pad the kernel so that nothing is lost due to the shift
-    # (biggest shift among dims + 1 for safety)
+    # 先填充，不会因移位丢失信息
     kernel = np.pad(kernel, np.int(np.ceil(np.max(shift_vec))) + 1, 'constant')
-
-    # Finally shift the kernel and return
     return interpolation.shift(kernel, shift_vec)
 
 
-# These next functions are all interpolation methods. x is the distance from the left pixel center
-
-
+# 下面函数是插值方法， x表示距左边像素中心的距离
+# 三次样条插值
 def cubic(x):
     absx = np.abs(x)
     absx2 = absx ** 2
@@ -266,22 +202,21 @@ def cubic(x):
     return ((1.5*absx3 - 2.5*absx2 + 1) * (absx <= 1) +
             (-0.5*absx3 + 2.5*absx2 - 4*absx + 2) * ((1 < absx) & (absx <= 2)))
 
-
+# lanczos(a=2)插值
 def lanczos2(x):
     return (((np.sin(pi*x) * np.sin(pi*x/2) + np.finfo(np.float32).eps) /
              ((pi**2 * x**2 / 2) + np.finfo(np.float32).eps))
             * (abs(x) < 2))
-
-
+# 分段插值
 def box(x):
     return ((-0.5 <= x) & (x < 0.5)) * 1.0
 
-
+# lanczos(a=3)插值
 def lanczos3(x):
     return (((np.sin(pi*x) * np.sin(pi*x/3) + np.finfo(np.float32).eps) /
             ((pi**2 * x**2 / 3) + np.finfo(np.float32).eps))
             * (abs(x) < 3))
 
-
+# 线性插值
 def linear(x):
     return (x + 1) * ((-1 <= x) & (x < 0)) + (1 - x) * ((0 <= x) & (x <= 1))
