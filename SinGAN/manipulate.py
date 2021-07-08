048beb225b7cd9d314eab5b920d85d3ef976d9c1
SHAN <1045117960@qq.com>
2021-07-08 19:29:06 +0800

    添加注释

--- SinGAN/manipulate.py
+++ SinGAN/manipulate.py
@@ -20,21 +20,28 @@ import matplotlib.pyplot as plt
 from SinGAN.training import *
 from config import get_arguments
 
+# 生成图像
+# n表示开始注入的层数
 def SinGAN_generate(Gs,Zs,reals,NoiseAmp,opt,in_s=None,scale_v=1,scale_h=1,n=0,gen_start_scale=0,num_samples=50):
     #if torch.is_tensor(in_s) == False:
     if in_s is None:
         in_s = torch.full(reals[0].shape, 0, device=opt.device)
     images_cur = []
+    # 遍历训练好的金字塔 从第n层之后的每一层
     for G,Z_opt,noise_amp in zip(Gs,Zs,NoiseAmp):
+        # 边界补零操作
         pad1 = ((opt.ker_size-1)*opt.num_layer)/2
         m = nn.ZeroPad2d(int(pad1))
+        # 计算该层噪声图像的长宽
         nzx = (Z_opt.shape[2]-pad1*2)*scale_v
         nzy = (Z_opt.shape[3]-pad1*2)*scale_h
 
-        images_prev = images_cur
-        images_cur = []
+        images_prev = images_cur # 存放生成后的图像（该层之前）
+        images_cur = [] #当层图像
+
 
         for i in range(0,num_samples,1):
+            # 生成该层计算出来的指定尺寸的噪声图像
             if n == 0:
                 z_curr = functions.generate_noise([1,nzx,nzy], device=opt.device)
                 z_curr = z_curr.expand(1,3,z_curr.shape[2],z_curr.shape[3])
@@ -43,11 +50,14 @@ def SinGAN_generate(Gs,Zs,reals,NoiseAmp,opt,in_s=None,scale_v=1,scale_h=1,n=0,g
                 z_curr = functions.generate_noise([opt.nc_z,nzx,nzy], device=opt.device)
                 z_curr = m(z_curr)
 
+            # I_prev表示上一层的输出
+            # 如果刚开始注入，该层的上一层输出就是in_s
             if images_prev == []:
                 I_prev = m(in_s)
                 #I_prev = m(I_prev)
                 #I_prev = I_prev[:,:,0:z_curr.shape[2],0:z_curr.shape[3]]
                 #I_prev = functions.upsampling(I_prev,z_curr.shape[2],z_curr.shape[3])
+            # 否则是上一层输出上采样到该层尺寸大小的图像
             else:
                 I_prev = images_prev[i]
                 I_prev = imresize(I_prev,1/opt.scale_factor, opt)
@@ -62,10 +72,14 @@ def SinGAN_generate(Gs,Zs,reals,NoiseAmp,opt,in_s=None,scale_v=1,scale_h=1,n=0,g
             if n < gen_start_scale:
                 z_curr = Z_opt
 
+            # 该层输入图像是 噪声分布 加 上一层生成器的输出上采样的图像
             z_in = noise_amp*(z_curr)+I_prev
+            # 经过生成器输出图像
             I_curr = G(z_in.detach(),I_prev)
 
+            # 如果到了金字塔最后一层
             if n == len(reals)-1:
+                # 保存路径
                 if opt.mode == 'train':
                     dir2save = '%s/RandomSamples/%s/gen_start_scale=%d' % (opt.out, opt.input_name[:-4], gen_start_scale)
                 else:
@@ -74,7 +88,9 @@ def SinGAN_generate(Gs,Zs,reals,NoiseAmp,opt,in_s=None,scale_v=1,scale_h=1,n=0,g
                     os.makedirs(dir2save)
                 except OSError:
                     pass
+                # num_samples=1,所以训练时保存图像到{RandomSamples/gen_start_scale=0}
                 if (opt.mode != "harmonization") & (opt.mode != "editing") & (opt.mode != "SR") & (opt.mode != "paint2image"):
+                    # vmin和vmax是限制图像颜色值的最小值和最大值
                     plt.imsave('%s/%d.png' % (dir2save, i), functions.convert_image_np(I_curr.detach()), vmin=0,vmax=1)
                     #plt.imsave('%s/%d_%d.png' % (dir2save,i,n),functions.convert_image_np(I_curr.detach()), vmin=0, vmax=1)
                     #plt.imsave('%s/in_s.png' % (dir2save), functions.convert_image_np(in_s), vmin=0,vmax=1)
